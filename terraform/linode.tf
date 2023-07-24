d30072ee3addd1c92816d92f92c08667a1abe56d
adamkoro <adam.koronics@gmail.com>
2023-07-24 18:27:02 +0200

    fix: ssh key from string variable

--- terraform/linode.tf
+++ terraform/linode.tf
@@ -3,7 +3,7 @@ resource "linode_instance" "harbor_build" {
   label           = var.linode_label
   region          = var.linode_region
   type            = var.linode_type
-  authorized_keys = var.linode_authorized_keys
+  authorized_keys = ["${var.linode_authorized_keys}"]
   root_pass       = var.linode_root_pass
 }
 
