90f8120658da5ab037a79d1807d005297b0cb765
adamkoro <adam.koronics@gmail.com>
2023-07-24 17:39:21 +0200

    feat: terraform

---
+++ terraform/linode.tf
@@ -0,0 +1,16 @@
+resource "linode_instance" "harbor_build" {
+        image = var.linode_image
+        label = var.linode_label
+        region = var.linode_region
+        type = var.linode_type
+        authorized_keys = var.linode_authorized_keys
+        root_pass = var.linode_root_pass
+}
+
+# Path: terraform/outputs.tf
+# Compare this snippet from terraform/outputs.tf:
+output "ip_address" {
+    value = linode_instance.harbor_build.ip_address
+}
+
+
---
+++ terraform/variables.tf
@@ -0,0 +1,43 @@
+# linode variables.tf
+variable "linode_token" {
+    description = "Linode API Token"
+    type = string
+    sensitive = true
+}
+
+variable "linode_region" {
+    description = "Linode Region"
+    type = string
+    default = "eu-central"
+}
+
+variable "linode_type" {
+    description = "Linode Type"
+    type = string
+    default = "g6-standard-2"
+}
+
+variable "linode_image" {
+    description = "Linode Image"
+    type = string
+    default = "linode/ubuntu22.04"
+}
+
+variable "linode_label" {
+    description = "Linode Label"
+    type = string
+    default = "harbor-build"
+}
+
+variable "linode_root_pass" {
+    description = "Linode Root Password"
+    type = string
+    sensitive = true
+}
+
+variable "linode_authorized_keys" {
+    description = "Linode Authorized Keys"
+    type = list(string)
+    default = []
+}
+
