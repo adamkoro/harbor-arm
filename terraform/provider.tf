64346347e2f91c1d12fe671d797c4cb36a3db730
adamkoro <adam.koronics@gmail.com>
2023-08-16 20:04:04 +0200

    chore: lint

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,13 @@
 terraform {
+  required_version = "1.5.5"
   required_providers {
     linode = {
       source  = "linode/linode"
       version = "2.5.2"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
     }
   }
 }
